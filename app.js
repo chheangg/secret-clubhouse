272c3c455deeb50e116fb40370265b25f0c82e09
chheangg <balldremz@gmail.com>
2022-12-20 09:41:58 +0700

    Setup folder for project

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Required Modules
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// convention in this project is using ES6's async rather than async module
+require('express-async-errors');
 
-var app = express();
+// Routes
+
+// Utilities
+const config = require('./utility/config');
+
+// MongoDB Config
+const db = mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+db.on('error', error => {
+  console.error('Error in MongoDB connection:' + error);
+})
+
+// Main express app
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +33,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes middleware
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ utility/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const nconf = require('nconf').argv().env()
+
+const PORT = nconf.get('PORT')
+const NODE_ENV = nconf.get('NODE_ENV')
+const MONGODB_URI = NODE_ENV === 'test' ? nconf.get('MONGODB_URI_TEST') : nconf.get('MONGODB_URI')
+
+module.exports = {
+  PORT,
+  NODE_ENV,
+  MONGODB_URI
+}
