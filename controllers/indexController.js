ddf5f260a9e9c5a10530960038f9f995de3657d5
chheangg <balldremz@gmail.com>
2022-12-22 13:53:55 +0700

    Display messages on index and seperate permission

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,12 @@
+const Message = require('../models/message')
+
 // Display index page
-exports.index_page = (req, res) => {
-  res.render('index')
+exports.index_page = async (req, res) => {
+  const messages = req.user 
+  ?
+  await Message.find({}).populate('user')
+  :
+  await Message.find({}, { timestamp: 0, user: 0 }).populate('user')
+
+  res.render('index', { messages })
 }
